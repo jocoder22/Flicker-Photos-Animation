515bea444229c18f53aaa9cfb0e0b83e9e13ace9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-10 10:31:20 -0500

    feat:  Add js animation
     Changes to be committed:
    	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -18,20 +18,25 @@
 flickerUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 var imageArray = [];
 
-function ajaxCall2() {
+function ajaxCall2(url) {
   return new Promise(function(resolve, reject) {
-    var data = $.getJSON(flickerUrl, {
+    var data = $.getJSON(url, {
       tag: 'beach',
       format: 'json'
-    });
+    }).done((data) => {
+    $("#img1").attr("src", data.items[0].media.m);
+    $("#img2").attr("src", data.items[1].media.m);
+    $("#img3").attr("src", data.items[2].media.m);
+  });
     resolve(data);
     reject(error);
   });
 }
 
 
-ajaxCall2().then(data => {
-  var bdata =  data.items;
+ajaxCall2(flickerUrl).then(data => {
+  var bdata = data.items;
+  $("img1").attr("src", data.items[0].media.m);
   bdata.forEach(item => {
     imageArray.push(item.media.m)
   });
@@ -39,23 +44,33 @@ ajaxCall2().then(data => {
     alert('There is an error: Flickr API not responding \n' + error);
 })
 
-$(() => {
-  // function photoAnimation(imageClass, n){
-    var imageN = $('#img1');
-    var i = 0;
+
+
+// $(() => {
+//   var imageN = $("#img1");
+//   var i = 0;
+//   setInterval(() => {
+//     i = (i + 1) % imageArray.length;
+//     imageN.fadeOut(function() {
+//       $(this).attr("src", imageArray[i]);
+//       $(this).fadeIn();
+//     });
+//   }, 2000);
+// });
+
+
+function photoAnimation(image, n) {
+  $(() => {
+    var imageN = $(image);
+    var i = n;
     setInterval(() => {
       i = (i + 1) % imageArray.length;
-      imageN.fadeOut(function() {
-        $(this).attr("src", imageArray[i]);
-        $(this).fadeIn();
-      });
-    }, 2000);
-  // };
-//   photoAnimation(img1, 0);
-//   photoAnimation(img2, 1);
-//   photoAnimation(img3, 2);
-});
+      imageN.attr("src", imageArray[i]);
+    }, 1000);
+  });
+}
 
-
-
+photoAnimation('#img1', 0);
+photoAnimation('#img2', 1);
+photoAnimation('#img3', 2);
 console.log(imageArray);
