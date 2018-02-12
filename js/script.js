097eb22833efe7071a12625a45dec298f0128b38
Okigbo <okigbookey@gmail.com>
2018-02-12 11:19:58 -0500

    Refactor: Add js iteration to Ajaxcall2
     Changes to be committed:
    	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function ajaxCall2(url) {
       tag: 'beach',
       format: 'json'
     }).done(data => {
-    $("#img1").attr("src", data.items[0].media.m);
-    $("#img2").attr("src", data.items[1].media.m);
-    $("#img3").attr("src", data.items[2].media.m);
+    for (var i = 0; i < 6; i++) {
+      $('#img'+ i).attr("src", data.items[i].media.m)
+    }
   });
     resolve(data);
     reject(error);
@@ -20,13 +20,12 @@ function ajaxCall2(url) {
 
 ajaxCall2(flickerUrl).then(data => {
   var bdata = data.items;
-  $("img1").attr("src", data.items[0].media.m);
   bdata.forEach(item => {
     imageArray.push(item.media.m)
   });
 }).catch(error => {
     alert('There is an error: Flickr API not responding \n' + error);
-})
+});
 
 
 function photoAnimation(image, n) {
@@ -36,13 +35,13 @@ function photoAnimation(image, n) {
     setInterval(() => {
       i = (i + 1) % imageArray.length;
       imageN.attr("src", imageArray[i]);
-    }, 1000);
+    }, 4000);
   });
 }
 
-photoAnimation('#img1', 0);
-photoAnimation('#img2', 1);
-photoAnimation('#img3', 2);
-photoAnimation('#img4', 17);
-photoAnimation('#img5', 18);
-photoAnimation('#img6', 19);
+photoAnimation('#img0', 0);
+photoAnimation('#img1', 1);
+photoAnimation('#img2', 2);
+photoAnimation('#img3', 19);
+photoAnimation('#img4', 18);
+photoAnimation('#img5', 17);
